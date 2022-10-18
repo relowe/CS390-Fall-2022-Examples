cd7ced416e419fba6be5027b002938732e531216
Robert Lowe <rlowe@semo.edu>
2022-10-18 13:36:35 -0500

    more parsing

--- calc/parser.py
+++ calc/parser.py
@@ -7,9 +7,34 @@ To write a parser:
     1.) Construct the basic interface (lexer, next, has, must_be).
     2.) Convert each BNF rule into a mutually recursive function.
     3.) Add data structures to build the parse tree.
+
+To construct a parse tree:
+    1.) Identify the tree node types. (atomic or actions)
+    2.) Create a data structure which can represent trees with
+        arbitrary numbers of children.
+    3.) Modify each of the recursive descent functions to return
+        a parse tree.
 """
 import sys
+from enum import Enum, auto
 from lexer import Token,Lexer
+
+class ParseType(Enum):
+    PROGRAM = auto()
+    ATOMIC = auto()
+    INPUT = auto()
+    ASSIGN = auto()
+    ADD = auto()
+    SUB = auto()
+    MUL = auto()
+    DIV = auto()
+    POW = auto()
+
+
+class ParseTree:
+    def __init__(self, node_type=ParseType.PROGRAM):
+        self.node_type = node_type
+        self.children = []
 
 class Parser:
     """
@@ -56,7 +81,7 @@ class Parser:
         """
         Attempt to parse a program.
         """
-        self.__program()
+        return self.__program()
 
     ###########
     # From here on down, everything is calc specific
@@ -65,104 +90,188 @@ class Parser:
     def __program(self):
         self.__next()
 
+        tree = ParseTree(ParseType.PROGRAM)
+
         while not self.__has(Token.EOF):
-            self.__statement()
+            tree.children.append(self.__statement())
+        return tree
 
 
     def __statement(self):
         if self.__has(Token.VARIABLE):
+            leaf = self.__lexer.get_tok()
             self.__next()
-            self.__ao_expression()
+            left = ParseTree(ParseType.ATOMIC)
+            left.children.append(leaf)
+
+            node = self.__ao_expression(left)
+            return node
         elif self.__has(Token.LPAREN):
             self.__next()
-            self.__expression()
+            node = self.__expression()
             self.__must_be(Token.RPAREN)
             self.__next()
-        elif self.__has(Token.INTLIT):
-            self.__next()
-            self.__factor2()
-            self.__term2()
-            self.__expression2()
-        elif self.__has(Token.FLOATLIT):
-            self.__next()
-            self.__factor2()
-            self.__term2()
-            self.__expression2()
-        else:
-            self.__must_be(Token.INPUT)
-            self.__input()
 
-    def __ao_expression(self):
+            return node
+        elif self.__has(Token.INPUT):
+            return self.__input()
+        else:
+            return self.__expression()
+
+
+    def __ao_expression(self, lv):
         if self.__has(Token.EQUAL):
             self.__next()
-            self.__expression()
+            node = ParseTree(ParseType.ASSIGN)
+            node.children.append(lv)
+            node.children.append(self.__expression())
+            return node
+
         else:
-            self.__factor2()
-            self.__term2()
-            self.__expression2()
+            node = lv
+            left1 = self.__factor2()
+            if left1:
+                left1.children.insert(0, node)
+                node = left1
+            left2 = self.__term2()
+            if left2:
+                left2.children.insert(0, node)
+                node = left2
+            left3 = self.__expression2()
+            if left3:
+                left3.children.insert(0, node)
+                node = left3
+            return node
 
 
     def __expression(self):
-        self.__term()
-        self.__expression2()
+        left = self.__term()
+        node = self.__expression2()
+        if node:
+            node.children.insert(0, left)
+        else:
+            node = left
+        return node
 
 
     def __expression2(self):
         if self.__has(Token.PLUS):
             self.__next()
-            self.__term()
-            self.__expression2()
+            node = ParseTree(ParseType.ADD)
+            right = self.__term()
+            right2 = self.__expression2()
+            if right2:
+                right2.children.insert(0, right)
+                right = right2
+            node.children.append(right)
+            return node
         elif self.__has(Token.MINUS):
             self.__next()
-            self.__term()
-            self.__expression2()
+            node = ParseTree(ParseType.SUB)
+            right = self.__term()
+            right2 = self.__expression2()
+            if right2:
+                right2.children.insert(0, right)
+                right = right2
+            node.children.append(right)
+            return node
+        else:
+            return False
 
     def __term(self):
-        self.__factor()
-        self.__term2()
+        left = self.__factor()
+        node = self.__term2()
+        if node:
+            node.children.insert(0, left)
+        else:
+            node = left
+        return node
 
 
     def __term2(self):
         if self.__has(Token.TIMES):
             self.__next()
-            self.__factor()
-            self.__term2()
+
+            node = ParseTree(ParseType.MUL)
+            right = self.__factor()
+            right2 = self.__term2()
+            if right2:
+                right2.children.insert(0,right)
+                right = right2
+            node.children.append(right)
+            return node
+
         elif self.__has(Token.DIVIDE):
             self.__next()
-            self.__factor()
-            self.__term2()
+
+            node = ParseTree(ParseType.DIV)
+            right = self.__factor()
+            right2 = self.__term2()
+            if right2:
+                right2.children.insert(0,right)
+                right = right2
+            node.children.append(right)
+            return node
+
+        else: 
+            return False
+
 
     def __factor(self):
-        self.__exponent()
-        self.__factor2()
+        left = self.__exponent()
+        node = self.__factor2()
+        if node:
+            node.children.insert(0, left)
+        else:
+            node = left
+        return node
 
 
     def __factor2(self):
         if self.__has(Token.POW):
             self.__next()
-            self.__factor()
+
+            node = ParseTree(ParseType.POW)
+            node.children.append(self.__factor())
+            return node
+        else:
+            return False
+
 
     def __exponent(self):
         if self.__has(Token.LPAREN):
             self.__next()
-            self.__expression()
+            node = self.__expression()
             self.__must_be(Token.RPAREN)
             self.__next()
-        elif self.__has(Token.VARIABLE):
+            return node
+        elif self.__has(Token.VARIABLE) or self.__has(Token.INTLIT) or self.__must_be(Token.FLOATLIT):
+            leaf = self.__lexer.get_tok() 
             self.__next()
-        elif self.__has(Token.INTLIT):
-            self.__next()
-        elif self.__must_be(Token.FLOATLIT):
-            self.__next()
+            node = ParseTree(ParseType.ATOMIC)
+            node.children.append(leaf)
+
 
     def __input(self):
         self.__must_be(Token.INPUT)
         self.__next()
+
+
         self.__must_be(Token.VARIABLE)
+
+        # build the node
+        node = ParseTree(ParseType.INPUT)
+        leaf = self.__lexer.get_tok()
+        left = ParseTree(ParseType.ATOMIC)
+        left.children.append(leaf)
+        node.children.append(left)
+
         self.__next()
+        return node
 
 
 # unit test 
 if __name__ == "__main__":
     p = Parser(Lexer())
-    p.parse()
+    tree = p.parse()
+    print(tree.children)
